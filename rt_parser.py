012a8972bca1fd5a7634e915fe0314e68775159e
sorooshi <sr.shalileh@gmail.com>
2024-01-25 18:54:56 +0300

    get all urls

--- rt_parser.py
+++ rt_parser.py
@@ -2,19 +2,18 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_urls(url: str, page_range: int) -> list:
-    urls = []
-    for page in range(page_range):
-        requested_urls = requests.get(url + str(page))
-        soup = BeautifulSoup(requested_urls, "html.parser")
-        for j in soup.find_all("a"):
-            link = j.get("href")
-            if not link in urls:
-                print(link)
-                urls.append(link)
+def get_urls(ref_url: str, max_page_range: int) -> list:
+    ref_url = ref_url + str(max_page_range)
+    requested_urls = requests.get(ref_url)
+    soup = BeautifulSoup(requested_urls.text, "html.parser")
+    urls = list()
+    for url in soup.find_all("a"):
+        link = url.get("href")
+        if link not in urls:
+            urls.append(link)
     return urls
 
-urls = get_urls("https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh?page=", 3)
+urls = get_urls("https://www.rottentomatoes.com/browse/movies_at_home/?page=", 4)
 print(urls)
 print(len(urls))
 
