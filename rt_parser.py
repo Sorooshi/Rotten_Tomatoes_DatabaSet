045df0a27589097a818487325c90c5f2a86c8bf1
sorooshi <sr.shalileh@gmail.com>
2024-01-26 22:18:57 +0300

    scrape per genre

--- rt_parser.py
+++ rt_parser.py
@@ -7,6 +7,48 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+""" 
+"ENTERTAINMENT",  "FAITH & SPIRITUALITY", "HEALTH & WELLNESS", 
+"HOUSE & GARDEN", "NATURE", "NEWS", "REALITY", "SOAP", "SPECIAL INTEREST", 
+"TALK SHOW", "TRAVEL", "VARIETY",   were empty
+"SHORT" has one movie
+"""
+genres = [
+    "action", "adventure", "animation", "anime", "biography", "comedy", "crime", 
+    "documentary", "drama", "fantasy", "lgbtq", "history", "holiday", "horror", 
+    "kids_and_family", "music", "musical", "mystery_and_thriller", "romance", 
+    "sci_fi", "sports", "stand_up", "war", "western"
+]
+
+urls_to_scrape = [
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:action~sort:popular", 
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:adventure~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:animation~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:anime~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:biography~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:comedy~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:crime~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:documentary~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:drama~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:fantasy~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:lgbtq~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:history~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:holiday~sort:popular"
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:horror~sort:popular", 
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:kids_and_family~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:music~sort:popular", 
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:musical~sort:popular", 
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:mystery_and_thriller~sort:popular",
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:romance~sort:popular", 
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:sci_fi~sort:popular", 
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:sports~sort:popular", 
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:stand_up~sort:popular", 
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:war~sort:popular", 
+    "https://www.rottentomatoes.com/browse/movies_at_home/critics:certified_fresh~genres:western~sort:popular", 
+]
+
+
+print(len(genres), len(urls_to_scrape))
 
 def get_urls_2(base_url: str, max_page_range: int) -> list:
 
@@ -43,10 +85,6 @@ def get_urls(base_url: str, max_page_range: int) -> list:
             url = link.get_attribute("href")  # "text", etc.
             if url not in urls:
                 urls.append(url)
-        # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button#onetrust-accept-btn-handler"))).click()
-        # WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a[title='Load more']"))).click()
-        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//button[@id='onetrust-accept-btn-handler']"))).click()
-        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//a[@title='Load more']"))).click()
         driver.quit()
 
         # except:
