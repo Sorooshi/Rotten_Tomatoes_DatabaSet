c0ed7530b59a60467982da3efe1f8e1299b9faa9
sorooshi <sr.shalileh@gmail.com>
2024-02-01 22:07:53 +0300

    semi final test

--- rt_scraper.py
+++ rt_scraper.py
@@ -1,23 +1,21 @@
-import os
 import pickle
-import requests 
 import numpy as np
 import pandas as pd 
-from bs4 import BeautifulSoup
+from tqdm import trange
 from selenium import webdriver
 from selenium.webdriver import FirefoxOptions
 
-GENRES = [
-    "action", "adventure", "animation", "anime", "biography", "comedy", "crime", 
-    "documentary", "drama", "fantasy", "lgbtq", "history", "holiday", "horror", 
-    "kids_and_family", "music", "musical", "mystery_and_thriller", "romance", 
-    "sci_fi", "sports", "stand_up", "war", "western"
-]
 
 def get_pickled_urls_all_genres(path: str) -> pickle:
     with open (path + ".pickle", "rb") as fp:
         genres_urls = pickle.load(fp)
     return genres_urls
+
+def get_all_collected_urls(path):
+    with open(path + ".txt", "r") as fp:
+        all_urls = fp.readlines()
+
+    return all_urls
 
 
 def get_data_per_url(ref_url: str) -> callable:
@@ -82,42 +80,39 @@ if __name__ == "__main__":
 
     # create an initial data frame to store the extracted information
     movie_data_df = pd.DataFrame(
-        index=np.arange(int(len(GENRES)*150)), 
+        # index=np.arange(int(len(GENRES)*150)), 
         columns=[
             "Title", "Synopsis", "Rating", "Genre", "Original Language", "Director", "Producer", "Writer", 
             "Release Date (Theaters)", "Release Date (Streaming)", "Box Office (Gross USA)", "Runtime", "Distributor", "Production Co", 
-            "Sound Mix", "Top Cast", "Aspect Ratio", "View the collection", "Link", "Initial Genre", 
+            "Sound Mix", "Top Cast", "Aspect Ratio", "View the collection", "Link", 
             ]
         )
     
-    all_genres_urls = get_pickled_urls_all_genres(path="urls_per_genres")
+    all_urls = get_all_collected_urls(path="collected_urls")
     issues = list()
     idx = 0
-    for k, v in all_genres_urls.items():
-        print(f" Genre: {k}")
-        for kk, vv in v.items():
-            tmp_title = kk.title().replace("_", " ")
-            print(
-                f"Title: {tmp_title}, "
-                f"Link: {vv}"
+    for u in trange(len(all_urls)):
+        url = all_urls[u]
+        tmp_title = url.split("m/")[-1].title().replace("_", " ")
+        print(
+            f"Title: {tmp_title}, "
+            f"Link: {url}"
             )
-            rts = RTScraper(vv)
-            synopsis, movie_info, top_casts = rts.get_all_required_info()
-            try:
-                movie_data_df.loc[idx, "Link"] = vv
-                movie_data_df.loc[idx, "Title"] = tmp_title
-                movie_data_df.loc[idx, "Initial Genre"] = k
-                movie_data_df.loc[idx, "Synopsis"] = synopsis
-                movie_data_df.loc[idx, "Top Cast"] = top_casts
-                
-                for kkk, vvv in movie_info.items():
-                    movie_data_df.loc[idx, kkk] = vvv
-            except:
-                print(f" There is an issue in {vv}")
-                issues.append(vv)
+        rts = RTScraper(url)
+        synopsis, movie_info, top_casts = rts.get_all_required_info()
+        try:
+            movie_data_df.loc[idx, "Link"] = url
+            movie_data_df.loc[idx, "Title"] = tmp_title
+            movie_data_df.loc[idx, "Synopsis"] = synopsis
+            movie_data_df.loc[idx, "Top Cast"] = top_casts
 
-            idx += 1
+            for kk, vv in movie_info.items():
+                movie_data_df.loc[idx, kk] = vv
+        except:
+            print(f" There is an issue in {url}")
+            issues.append(url)
 
+        idx += 1
 
     movie_data_df.to_csv("rotten_tomatoes_info.csv")
     with open ("issues.txt", "w") as fp:
