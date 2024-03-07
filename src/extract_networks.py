55d0e10a01873b904f65b0d7b738e58e34849427
sorooshi <sr.shalileh@gmail.com>
2024-03-07 13:35:56 +0300

    get Med. Adj.

---
+++ src/extract_networks.py
@@ -0,0 +1,83 @@
+import numpy as np
+import pandas as pd
+
+
+def get_list_of_casts(x):
+    x = x.split(", ")
+    xx = []
+    for k in range(len(x)):
+        if k == 0:
+            xx.append(x[k].split("[")[1])
+        elif k == len(x):
+            xx.append(x.split("]")[0])
+        else:
+            xx.append(x[k])
+    return set(xx)
+
+def get_list_of_others(x):
+    return set(x.split(", "))
+
+def get_edge_weight(a, b):
+    return ((len(a.intersection(b)) / len(a)) + len(b.intersection(a)) / len(b)) / 2
+
+def get_medium_adjacency_matrix(df: pd.DataFrame) -> pd.DataFrame:
+    df_np = df.values
+    adjacency = np.zeros(shape=(len(df_np, len(df_np))))
+
+    for i in range(len(df_np)):
+        for j in range(len(df_np)):
+            if i != j:
+                # Directors
+                weight_dir = get_edge_weight(
+                a = get_list_of_others(df_np[i, 4]), 
+                b = get_list_of_others(df_np[j, 4])
+            )
+                # Producers
+                weight_pro = get_edge_weight(
+                    a = get_list_of_others(df_np[i, 5]), 
+                    b = get_list_of_others(df_np[j, 5])
+                )
+                # Writers
+                weight_wri = get_edge_weight(
+                    a = get_list_of_others(df_np[i, 6]), 
+                    b = get_list_of_others(df_np[j, 6])
+                )
+                # Top casts
+                weight_casts = get_edge_weight(
+                    a = get_list_of_casts(df_np[i, 7]), 
+                    b = get_list_of_casts(df_np[j, 7])
+                )
+                weight = weight_dir + weight_pro + weight_wri + weight_casts
+        else:
+            weight = 0.
+            
+        adjacency[i, j] = weight
+
+
+    data_df_a = pd.DataFrame(
+        data=adjacency, 
+        columns=df.Title.values,
+        index=df.Title.values,
+        )
+    
+    data_a = pd.DataFrame(
+        data=adjacency, 
+        )
+    
+    data_df_a.to_csv("./data/medium_data_df_a.csv", index=False)
+    data_a.to_csv("./data/medium_data_a.csv", index=False)
+
+    return data_df_a, data_a
+
+
+if __name__ == "__main__":
+
+    medium_df = pd.read_csv('./data/medium_movies_data.csv')
+    print(
+        f"Medium data size: {medium_df.shape}", 
+        f"Medium data head: {medium_df.head()}"
+        )
+
+    data_df_a, data_a = get_medium_adjacency_matrix(df=medium_df)
+    
+    
