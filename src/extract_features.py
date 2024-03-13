8789884e5a0b39888e43dca7a0b310118d372abb
sorooshi <sr.shalileh@gmail.com>
2024-03-13 14:23:23 +0300

    LSTM-AE basic

---
+++ src/extract_features.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import keras_tuner as kt 
+
+tfk = tf.keras 
+tfkl = tf.keras.layers
+
+
+class LstmAe(tfk.Model):
+    def __init__(self, latent_dim, embedding_dim, vocab_size):
+        super().__init__()
+
+        # self.latent_dim = latent_dim
+        # self.embedding_dim = embedding_dim
+        # self.vocab_size = vocab_size
+
+        self.input = tfkl.Input(
+            shape=(1, ), dtype=tf.string
+            )
+
+        self.txt_vec = tfkl.TextVectorization(
+            max_tokens=vocab_size, 
+            split="whitespace", ngrams=1, 
+            output_mode="int", ragged=True,
+            standardize="lower_and_strip_punctuation",
+            )
+        self.emb = tfkl.Embedding(
+            input_dim=len(self.txt_vec.get_vocabulary()),
+            output_dim=latent_dim,
+            )
+        self.enc = tfkl.Bidirectional(
+            tfkl.LSTM(
+                units=latent_dim,  # hp.Int('units', min_value=2, max_value=100, step=5), 
+                activation="relu",  # hp.Choice("activation", ["relu", "tanh"]), 
+                # dropout=hp.Float('dropout', min_value=0.0, max_value=0.5, step=0.1),
+                name="encoder 1"
+            )
+        )
+
+        self.dec1 = tfkl.Bidirectional(
+            tfkl.LSTM(
+                units=10,  # hp.Int('units', min_value=2, max_value=100, step=5), 
+                activation="relu",  # hp.Choice("activation", ["relu", "tanh"]), 
+                # dropout=hp.Float('dropout', min_value=0.0, max_value=0.5, step=0.1),
+                name="decoder 1"
+            )
+        )
+
+        self.dec2 = tfkl.Bidirectional(
+            tfkl.LSTM(
+                units=20,  # hp.Int('units', min_value=2, max_value=100, step=5), 
+                activation="tanh",  # hp.Choice("activation", ["relu", "tanh"]), 
+                # dropout=hp.Float('dropout', min_value=0.0, max_value=0.5, step=0.1),
+                name="decoder 2"
+            )
+        )
+
+    def call(self, inputs):
+        x = self.input(inputs)
+        x = self.txt_vec().adapt(data=x, batch_size=8, steps=None)
+        x = self.emb(x)
+        x = self.enc(x)
+        x = self.dec1(x)
+        x = self.dec2(x)
+        return x
+    
+
+
+class 
+
+
+
