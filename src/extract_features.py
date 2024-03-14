82be5e9ea96511eaa8feaa62ffdff600b823c87b
sorooshi <sr.shalileh@gmail.com>
2024-03-14 12:21:22 +0300

    input LA: #1

--- src/extract_features.py
+++ src/extract_features.py
@@ -16,7 +16,7 @@ class LstmAe(tfk.Model):
         # self.vocab_size = vocab_size
 
         self.inputs = tfkl.Input(
-            shape=(None, 140), dtype=tf.string
+            shape=(None, 12,), dtype=tf.string
         )
 
         self.txt_vec = tfkl.TextVectorization(
