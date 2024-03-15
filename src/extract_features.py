608120c8d05a84f56fdf1badff2e7f9ee27283f5
sorooshi <sr.shalileh@gmail.com>
2024-03-15 18:36:26 +0300

    compute_loss()

--- src/extract_features.py
+++ src/extract_features.py
@@ -83,7 +83,7 @@ class LstmAe(tfk.Model):
 
         with tf.GradientTape() as tape:
             y_pred = self(x, training=True)
-            loss = self.compute_loss(self.y, y_pred) 
+            loss = self.loss(self.y, y_pred) 
         
         trainable_vars = self.trainable_variables
         gradients = tape.gradient(loss, trainable_vars)
