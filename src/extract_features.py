b2fcc0d034974d5f8ad0e941676234d2571bc2ef
sorooshi <sr.shalileh@gmail.com>
2024-03-19 20:39:36 +0300

    fine-tuner: Input

--- src/extract_features.py
+++ src/extract_features.py
@@ -253,7 +253,7 @@ class FineTuneLstmAe(TrainTestLstmAe):
 
         model = tfk.Sequential()
         model.add(
-            tfkl.Input(input_shape=(1,), dtype=tf.string,)
+            tfkl.Input(shape=(1,), dtype=tf.string,)
         )
         model.add(
             tfkl.TextVectorization(
