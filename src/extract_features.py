c3d9eb3d41626b37bcc6b1ea0ad001ad0631f82b
sorooshi <sr.shalileh@gmail.com>
2024-03-20 18:39:47 +0300

    tuner: beguging

--- src/extract_features.py
+++ src/extract_features.py
@@ -312,7 +312,7 @@ class FineTuneLstmAe(TrainTestLstmAe):
 
         model = tfk.Sequential()
         model.add(
-            tfkl.Input(shape=(self.max_seq_len,), dtype=tf.string,)
+            tfkl.Input(shape=(1,), dtype=tf.string,)
         )
         model.add(
             tfkl.TextVectorization(
