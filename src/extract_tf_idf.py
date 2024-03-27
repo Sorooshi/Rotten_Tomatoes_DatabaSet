cee9d066003dac2c8a5ecbac387aba7f8a947341
sorooshi <sr.shalileh@gmail.com>
2024-03-27 18:34:43 +0300

    init tf-Idf

---
+++ src/extract_tf_idf.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import nltk
+import pickle
+import string
+import itertools
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from nltk import word_tokenize          
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
