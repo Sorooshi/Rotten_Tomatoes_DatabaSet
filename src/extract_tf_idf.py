8556a20fd555e809be762d5d3e275bd131cf93f1
sorooshi <sr.shalileh@gmail.com>
2024-03-28 16:07:08 +0300

    Tf-Idf: F

--- src/extract_tf_idf.py
+++ src/extract_tf_idf.py
@@ -172,6 +172,12 @@ if __name__ == "__main__":
         max_features=1000, 
     )
 
-    print("data_x_df is extracted!")
+    df = tfidf_getter.get_feature_data()
+    
+    print(
+        f"df.head: {df.head()} \n"
+        f"df.describe: {df.describe()} \n"
+        f"df.shape: {df.shape} \n"
+    )
 
 
