c155f9028ded64bb468736519262e1f93237b83c
sorooshi <sr.shalileh@gmail.com>
2024-03-28 16:36:18 +0300

    save with titles

--- src/extract_text_embedding.py
+++ src/extract_text_embedding.py
@@ -500,12 +500,14 @@ class TuneApplyLstmAe():
                 )
             )
             if self.data_name == "medium_movies_data":
-                features = ["Runtime", "Box Office (Gross USA)", "Tomato Meter", "Audience Score", 
-                "No. Reviews", "Genre"
+                features = [
+                    "Title", "Runtime", "Box Office (Gross USA)", "Tomato Meter", 
+                    "Audience Score", "No. Reviews", "Genre"
                 ]
             elif self.data_name == "large_movies_data":
-                features = ["Runtime", "Tomato Meter", "Audience Score", 
-                "No. Reviews", "Genre"
+                features = [
+                    "Title", "Runtime", "Tomato Meter", 
+                    "Audience Score", "No. Reviews", "Genre"
                 ]
 
             features += ["Embedding-"+ str(f) for f in range(embedding_features.shape[1])]
@@ -514,10 +516,12 @@ class TuneApplyLstmAe():
             
             if self.data_name == "medium_movies_data":
                 data_df_x.to_csv("./data/medium_data_df_x.csv", index=True, columns=features)
+                data_df_x = data_df_x[features[1:]]  # dropping titles
                 data_df_x.to_csv("./data/medium_data_x.csv", header=False, index=False)
 
             elif self.data_name == "large_movies_data":
                 data_df_x.to_csv("./data/large_data_df_x.csv", index=True, columns=features)
+                data_df_x = data_df_x[features[1:]]  # dropping titles
                 data_df_x.to_csv("./data/large_data_x.csv", header=False, index=False)
 
             
