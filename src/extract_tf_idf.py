dcd1b163b988ab0d30ce00978070004e8fbad1b2
sorooshi <sr.shalileh@gmail.com>
2024-03-28 13:59:25 +0300

    Tf-Idf: SF

--- src/extract_tf_idf.py
+++ src/extract_tf_idf.py
@@ -1,4 +1,5 @@
 import os
+import re
 import sys
 import nltk
 import pickle
@@ -9,6 +10,96 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from nltk import word_tokenize          
 from nltk.stem import WordNetLemmatizer
+from extract_text_embedding import GetConvertedData
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
+
+class ExtractTfIdf():
+    def __init__(self, corpus, max_n_features, *args, **kwargs):
+        self.corpus = corpus
+        self.max_n_features = max_n_features
+        self.wnl = WordNetLemmatizer()
+        self.documents = list()
+        self.vocabulary = dict()
+
+        user_defined_stopwords = ["st","rd","hong","kong", "...", ] 
+        a = nltk.corpus.stopwords.words('english')
+        b = list(string.punctuation) + user_defined_stopwords
+        self.stopwords = set(a).union(b)
+
+    def preprocess(self, corpus, get_vocab):
+        """returns a list of sets, where each set is lowercased, stripped, 
+        splitted, and lemmatized version of the original the synopsis text."""
+
+        noises = ["$", ",", ".", ":", ";", "(", ")", "()" ]
+        
+        for document in corpus:
+            document = document.lower().strip().split()
+            document = [re.sub(r"\[a-z0-9]+", "", word)  for word in document]
+            for noise in noises:
+                document = [word.replace(noise, "")  for word in document]
+            document = [word for word in document if word not in self.stopwords]
+            document = [self.wnl.lemmatize(word) for word in document]
+            document = " ".join(document)
+            if get_vocab is not True:
+                document = " ".join(document)
+            self.documents.append(document)
+
+        return self.documents
+    
+
+    def get_vocabulary(self,):
+        """returns a dict where keys are word and values are the integer indices."""
+       
+        cntr = 0
+        for document in self.documents:
+            for word in document:
+                if word not in self.vocabulary.keys():
+                    self.vocabulary[word] = cntr
+                    cntr += 1
+        return self.vocabulary
+    
+    def get_tf_idf(self, max_features: int = 100, ngrams_rng: tuple = (2, 2),):
+
+        vectorizer = TfidfVectorizer(
+            input="content", 
+            tokenizer=None, 
+            strip_accents="ascii", 
+            decode_error="ignore",
+            stop_words=None,
+            lowercase=False,
+            max_df=10,
+            min_df=2,
+            ngram_range=ngrams_rng,
+            sublinear_tf=True,
+            max_features=max_features, 
+            sublinear_tf=False, 
+            smooth_idf=True,
+            vocabulary=None,
+        )
+        x = vectorizer.fit_transform(self.documents)
+
+        tf_idf = pd.DataFrame(
+            x.toarray(), index=self.corpus.Title, 
+            columns=vectorizer.get_feature_names()
+            )
+        
+        return tf_idf
+    
+
+    def get_feature_data(self, tf_idf, data_name):
+        
+        if data_name == "medium_movies_data":
+                features = ["Runtime", "Box Office (Gross USA)", "Tomato Meter", "Audience Score", 
+                "No. Reviews", "Genre"
+                ]
+        elif data_name == "large_movies_data":
+                features = ["Runtime", "Tomato Meter", "Audience Score", 
+                "No. Reviews", "Genre"
+                ]
+        
+        data_x_df = self.corpus[features]
+        data_x_df = pd.join(data_x_df, tf_idf)
+
+        return data_x_df
