a389a55d8310f6d44ff574c473d01153cc8f8783
sorooshi <sr.shalileh@gmail.com>
2024-03-29 11:22:28 +0300

    new search space after adding PP

--- src/extract_text_embedding.py
+++ src/extract_text_embedding.py
@@ -612,7 +612,7 @@ if __name__ == "__main__":
     else:
         # best config
         # lr, epochs, latent_dim, ngrams, max_seq_len
-        config = (1e-5, 2000, 20, 2, 100)
+        config = (1e-5, 2000, 20, 2, 75)
         tuner_applier.train_and_extract_features_tuned_model(configs=config)
 
     
