dcb8d1f91c4962b0cb61947d9e7a0a53b450681f
sorooshi <sr.shalileh@gmail.com>
2024-03-29 10:19:00 +0300

    ngrams := 2

--- src/extract_tf_idf.py
+++ src/extract_tf_idf.py
@@ -207,7 +207,7 @@ if __name__ == "__main__":
         data_df=data_df, 
         corpus=text_data, 
         max_features=max_features, 
-        ngrams_rng=(1, 1),
+        ngrams_rng=(2, 2),
 
     )
 
